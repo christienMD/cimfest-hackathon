81c0dff2c88381daa160b2a4bc79e059b68320a0
Md Christien <mdchristien12@gmail.com>
2024-11-11 09:31:53 +0100

    Build signup page

---
+++ src/components/sections/AuthLogo/AuthLogo.tsx
@@ -0,0 +1,17 @@
+
+const AuthLogo = () => {
+  return (
+    <div className="flex items-center justify-center gap-2 mb-6 md:mb-8">
+      <div className="p-0.5">
+        <img
+          src="https://www.figma.com/file/Hw24rKXR9xcsS8NyturKwh/image/8482786bdc2517476ff9f26bc62e5adbf3a5ceb8"
+          alt="Afro Coach Logo"
+          className="w-10 h-10 md:w-14 md:h-14"
+        />
+      </div>
+      <span className="font-bold text-2xl">AFRO COACH</span>
+    </div>
+  );
+}
+
+export default AuthLogo

7d5b415b7a794d3d8f6564922653e3b2e098c6a3
Md Christien <mdchristien12@gmail.com>
2024-11-12 07:47:48 +0100

    fix: fix linked pages

--- src/components/sections/GetStarted/GetStarted.tsx
+++ src/components/sections/GetStarted/GetStarted.tsx
@@ -10,7 +10,7 @@ const GetStarted = () => {
         borderRadius: "20px",
       }}
     >
-      <Link to="/login">
+      <Link to="/signup">
         <div className="absolute inset-0 rounded-[20px] bg-gradient-to-r from-[#411294] to-[#9C64FF] opacity-0 group-hover:opacity-20 transition-opacity" />
         <div className="h-full w-full flex items-center justify-center bg-gradient-to-r from-[#323052] to-[#411294] text-white rounded-[17px]">
           Sign Up
